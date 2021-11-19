f2594e6480ddc9e202698833c21454d0c96e2fb4
Thomas Honeyman <admin@thomashoneyman.com>
2021-11-19 11:33:24 -0500

    review

--- src/Pathy/Name.purs
+++ src/Pathy/Name.purs
@@ -88,11 +88,9 @@ alterExtension
    . (Maybe NonEmptyString -> Maybe NonEmptyString)
   -> Name n
   -> Name n
-alterExtension f n =
-  let
-    spn = splitName n
-  in
-    joinName spn { ext = f spn.ext }
+alterExtension f n = do
+  let spn = splitName n
+  joinName spn { ext = f spn.ext }
 
 -- | A class for creating `Name` values from type-level strings. This allows us
 -- | to guarantee that a name is not empty at compile-time.
